15930ca4ff43bc58e345cbf4e2b2184fc18c86b9
Kevin Hwangpo <hwangpo3@gmail.com>
2018-06-27 09:30:25 -0700

    setting up tests/understanding stack push pop peak

---
+++ src/__test__/stack.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Stack = require('../lib/stack');
+
+describe('stack.js', () => {
+  test('#constructor', () => {
+
+  });
+
+  test('#pop should kill top node of stack', () => {
+
+  });
+
+  test('#peak should look just at node on top', () => {
+
+  });
+
+  test('#push should insert value at head of linkedlist', () => {
+
+  });
+});
