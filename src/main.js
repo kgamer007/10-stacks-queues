35f6f5b8e43220ce4d9c262598f32cab0b38b787
Kevin Hwangpo <hwangpo3@gmail.com>
2018-06-27 22:29:31 -0700

    queue tests... I need help with

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 'use strict';
 
-require('./lib/recursion');
+/*eslint-disable*/
+const Stack = require('./lib/stack');
 const Queue = require('./lib/queue');
-
-const q = new Queue();
-q.enqueue(1);
-q.enqueue(2);
-q.enqueue(3);
-console.log(q, 'q with three elements');
-
-q.dequeue();
-console.log(q, 'q after dequeue');
-console.log(q.peek());
+const LinkedList = require('./lib/linked-list');
