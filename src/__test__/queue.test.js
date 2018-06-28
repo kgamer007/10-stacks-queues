216efaeb0c932d1d815a4dff8162150ee55e3df1
Kevin Hwangpo <hwangpo3@gmail.com>
2018-06-27 22:20:38 -0700

    finishing up tests on queue, not done yet

---
+++ src/__test__/queue.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Queue = require('../lib/queue');
+
+let qTest;
+
+beforeAll(() => {
+  qTest = new Queue();
+});
+
+describe('#enQueue TESTS', () => {
+  test('#constructor', () => {
+    const qTest = new Queue();
+  });
+  test('#enqueue is actually head ', () => {
+    const qTest = new Queue();
+  });
+  test('#engueue gets 1 more added', () => {
+    const qTest = new Queue();
+  });
+});
+
+describe('#deQueue TESTS', () => {
+  test('#constructor', () => {
+    const qTest = new Queue();
+  });
+  test('#dequeue loses 1 in list', () => {
+    const qTest = new Queue();
+  });
+  test('#dequeue is null', () => {
+    const qTest = new Queue();
+  });
+});
