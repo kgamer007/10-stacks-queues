ee1d57fe9410501c5cb07cbc31c444690374fbef
Kevin Hwangpo <hwangpo3@gmail.com>
2018-06-28 17:51:20 -0700

    finished adding test cases and finished queue module

--- src/__test__/queue.test.js
+++ src/__test__/queue.test.js
@@ -2,32 +2,60 @@
 
 const Queue = require('../lib/queue');
 
-let qTest;
+let myQ;
 
-beforeAll(() => {
-  qTest = new Queue();
+beforeEach(() => {
+  myQ = new Queue();
 });
 
-describe('#enQueue TESTS', () => {
+afterEach(() => {
+  myQ = null;
+});
+
+describe('#constructor: does it work?', () => {
   test('#constructor', () => {
-    const qTest = new Queue();
-  });
-  test('#enqueue is actually head ', () => {
-    const qTest = new Queue();
-  });
-  test('#engueue gets 1 more added', () => {
-    const qTest = new Queue();
+    expect(myQ.storage.head).toBeNull();
+    expect(myQ.storage.tail).toBeNull();
   });
 });
 
-describe('#deQueue TESTS', () => {
-  test('#constructor', () => {
-    const qTest = new Queue();
+describe('#Queue TESTS', () => {
+  describe('#enQueue TESTS', () => {
+    beforeEach(() => {
+      myQ.enqueue('kevin');
+      myQ.enqueue('Devin');
+      myQ.enqueue('Tracy');
+      myQ.enqueue('Noah');
+    });
+
+    test('#enqueue gets 1 more added', () => {
+      expect(myQ.storage.head.value).toEqual('kevin');
+      expect(myQ.storage.head.next.value).toEqual('Devin');
+      expect(myQ.storage.head.next.next.value).toEqual('Tracy');
+      expect(myQ.storage.head.next.next.next.value).toEqual('Noah');
+    });
   });
-  test('#dequeue loses 1 in list', () => {
-    const qTest = new Queue();
+
+  describe('#deQueue TESTS', () => {
+    test('#dequeue takes a node away', () => {
+      expect(myQ.storage.head).toBeNull();
+      myQ.enqueue(1);
+      myQ.enqueue(5);
+      myQ.enqueue(9);
+      expect(myQ.storage.head.value).toBe(1);
+      myQ.dequeue();
+      expect(myQ.storage.head.value).toBe(5);
+    });
   });
-  test('#dequeue is null', () => {
-    const qTest = new Queue();
+
+  describe('#peek TESTS', () => {
+    test('#peek looks front of the list', () => {
+      myQ.enqueue(1);
+      expect(myQ.storage.head.value).toEqual(1);
+    });
+
+    test('#peek is empty', () => {
+      expect(myQ.peek()).toBeNull();
+    });
   });
 });
